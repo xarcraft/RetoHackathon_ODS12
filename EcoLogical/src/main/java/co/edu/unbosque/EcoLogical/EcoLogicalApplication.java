f015c2a9e5987566c2037a1ac709b9d6aadc602d
Catherine Escobar <bettelgeuxe@hotmail.com>
2021-11-18 16:00:04 -0500

    Inicio proyecto

---
+++ EcoLogical/src/main/java/co/edu/unbosque/EcoLogical/EcoLogicalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.EcoLogical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoLogicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoLogicalApplication.class, args);
+	}
+
+}
