464403183585b519646feb1b8be2e3536c3393b5
Catherine Escobar <bettelgeuxe@hotmail.com>
2021-11-30 19:27:27 -0500

    Correccion estructura reto back

--- EcoLogical/src/main/java/co/edu/unbosque/EcoLogical/Model/Retos.java
+++ EcoLogical/src/main/java/co/edu/unbosque/EcoLogical/Model/Retos.java
@@ -12,12 +12,8 @@ public class Retos {
 	private String nombre_reto;
 	private String clave_reto;
 	private String descripcion_reto;
-	private String cedula_usuario;
 	private Long puntos_reto;
-	private Long subtotalpuntos;
-	private Long total_puntos;
 	private Long num_reto;
-	
 	
 	public String get_id() {
 		return _id;
@@ -25,7 +21,6 @@ public class Retos {
 	public void set_id(String _id) {
 		this._id = _id;
 	}
-	
 	public String getCodigo_reto() {
 		return codigo_reto;
 	}
@@ -50,29 +45,11 @@ public class Retos {
 	public void setDescripcion_reto(String descripcion_reto) {
 		this.descripcion_reto = descripcion_reto;
 	}
-	public String getCedula_usuario() {
-		return cedula_usuario;
-	}
-	public void setCedula_usuario(String cedula_usuario) {
-		this.cedula_usuario = cedula_usuario;
-	}
 	public Long getPuntos_reto() {
 		return puntos_reto;
 	}
 	public void setPuntos_reto(Long puntos_reto) {
 		this.puntos_reto = puntos_reto;
-	}
-	public Long getSubtotalpuntos() {
-		return subtotalpuntos;
-	}
-	public void setSubtotalpuntos(Long subtotalpuntos) {
-		this.subtotalpuntos = subtotalpuntos;
-	}
-	public Long getTotal_puntos() {
-		return total_puntos;
-	}
-	public void setTotal_puntos(Long total_puntos) {
-		this.total_puntos = total_puntos;
 	}
 	public Long getNum_reto() {
 		return num_reto;
@@ -80,6 +57,8 @@ public class Retos {
 	public void setNum_reto(Long num_reto) {
 		this.num_reto = num_reto;
 	}
+	
+	
 	
 	
 	
